4e202b060e4b4f13f5546a023478de93b68ccb9c
Jon <jonsilva026@gmail.com>
2023-03-13 11:09:33 -0300

    feat: adicionando titulo, subtitulo e os campos para pegar o valor da altura e peso

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import styles from "./App.module.css";
 import poweredImage from "./assets/powered.png";
 
 const App = () => {
+  const [heightField, setHeightField] = useState<number>(0);
+  const [weightField, setWeightField] = useState<number>(0);
+
   return (
     <div className={styles.main}>
       <header>
@@ -10,7 +14,27 @@ const App = () => {
         </div>
       </header>
       <div className={styles.container}>
-        <div className={styles.leftSide}>...</div>
+        <div className={styles.leftSide}>
+          <h1>Calcule o seu IMC.</h1>
+          <p>
+            IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela
+            Organização Mundial de Saúde para calcular o peso ideal de cada
+            pessoa.
+          </p>
+
+          <input
+            type="number"
+            placeholder="Digite a sua altura. Ex: 1.5 (em métros)"
+            value={heightField > 0 ? heightField : ""}
+            onChange={(e) => setHeightField(parseFloat(e.target.value))}
+          />
+          <input
+            type="number"
+            placeholder="Digite o seu peso. Ex: 75.3 (em kg)"
+            value={weightField > 0 ? weightField : ""}
+            onChange={(e) => setWeightField(parseFloat(e.target.value))}
+          />
+        </div>
         <div className={styles.rightSide}>...</div>
       </div>
     </div>
